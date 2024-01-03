339f7b704a12b00f5c239e4c3146b636a317c4dc
Crowdbotics <team@crowdbotics.com>
2024-01-03 19:02:12 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "shane-mckenna-app-45260",
+  url: "https://shane-mckenna-app-45260.botics.co",
+  api: "https://shane-mckenna-app-45260.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
